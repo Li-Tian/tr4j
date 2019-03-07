f47fb18bf06689855b2e2153c24c45dc3f18939d
luchuan <luchuan@neo.org>
2019-03-07 18:53:37 +0800

    1. add toBytes method on ByteEnum

--- src/main/java/neo/common/ByteEnum.java
+++ src/main/java/neo/common/ByteEnum.java
@@ -31,4 +31,19 @@ public interface ByteEnum {
         }
         throw new IllegalArgumentException();
     }
+
+    /**
+     * 将类型列表转化成byte数组
+     *
+     * @param ts  类型列表
+     * @param <T> 类型泛型
+     * @return byte[]
+     */
+    static <T extends Enum & ByteEnum> byte[] toBytes(T[] ts) {
+        byte[] values = new byte[ts.length];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = ts[i].value();
+        }
+        return values;
+    }
 }
