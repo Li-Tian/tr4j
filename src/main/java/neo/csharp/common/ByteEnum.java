367f1a39ef043de2f29d3f59d49031cb477e03d5
luchuan <luchuan@neo.org>
2019-04-15 15:42:09 +0800

    1. remove ByteEnum parse method

--- src/main/java/neo/csharp/common/ByteEnum.java
+++ src/main/java/neo/csharp/common/ByteEnum.java
@@ -1,7 +1,5 @@
 package neo.csharp.common;
 
-import neo.log.notr.TR;
-
 /**
  * Byte枚举类型定义
  */
@@ -16,24 +14,6 @@ public interface ByteEnum {
      */
     int BYTES = Byte.BYTES;
 
-    /**
-     * 从byte值中，解析出对应的类型
-     *
-     * @param ts   枚举范围
-     * @param type 类型值
-     * @param <T>  枚举泛型
-     * @return T
-     * @throws IllegalArgumentException 不存在的类型，当type值不存在时
-     */
-    static <T extends Enum & ByteEnum> T parse(T[] ts, byte type) {
-        TR.fixMe("Enum 数量很多时，这个方法效率很低。最好改写成查哈希表。");
-        for (T t : ts) {
-            if (t.value() == type) {
-                return t;
-            }
-        }
-        throw new IllegalArgumentException();
-    }
 
     /**
      * 将类型列表转化成byte数组
